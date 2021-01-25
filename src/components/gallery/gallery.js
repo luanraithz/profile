96ac8445a3e27ad248c95a3c13116703af3c7f2a
luanraithz <luanraithz@gmail.com>
2021-01-24 21:58:42 -0300

    Removed box-shadow¨

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -15,7 +15,6 @@ const Wrapper = styled.a`
     cursor: pointer;
     overflow: hidden;
     border-radius: 2.08333% / 4.6875%;
-    box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
 `
 
 const Image = styled.img`
