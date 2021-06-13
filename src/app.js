46c89deb05aea552c17966595de6b4a48851c471
luanraithz <luanraithz@gmail.com>
2021-06-13 19:59:26 -0300

    update to use raw css and add some style to generated html

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import { Header } from './components/header';
-// import { Gallery } from './components/gallery';
 import { UserProfile } from './components/body';
-import { createGlobalStyle } from 'styled-components';
 import { pageTitle } from './profile.json';
-import './styles.scss';
-import { mobileScreenSize } from './styleguide/breakpoints';
+import './styles.css';
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,38 +10,11 @@ import {
 } from "react-router-dom";
 import { Recomendations } from './pages/recomendations';
 
-import { gray, themeColor } from './styleguide/colors';
-const GlobalStyle = createGlobalStyle`
-  :root {
-    --header-height: 300px;
-  --theme-color: ${themeColor};
-  --text-color: ${gray};
-
-    @media screen and (max-width: ${mobileScreenSize}) {
-      --header-height: 200px;
-    }
-  }
-
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-
-  body, html, #app {
-    background-color: #262626;
-    font-family: 'Roboto', sans-serif;
-    padding:0;
-    margin:0;
-    height: 100%;
-  }
-`
 document.title = pageTitle;
 
 export const App = () => (
 
   <React.Fragment>
-    <GlobalStyle />
     <Router>
       <Header />
       <Switch>
--- src/blog/convert.js
+++ src/blog/convert.js
@@ -23,9 +23,47 @@ const formatted = folders.map(f => {
 formatted.sort((b1, b2) => new Date(b2.header.date) - new Date(b1.header.date))
 
 
+const css = fs.readFileSync(path.join(__dirname, "..", "styles.css"))
 formatted.forEach(c => {
 	const htmlContent = marked(c.content)
-	fs.writeFileSync(path.join(__dirname, c.path,"index.html"), htmlContent)
+
+	const date = new Date(c.header.date)
+	const m = date.toLocaleString("default", { month: "long" })
+	const content = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="Description" content="Luan Raithz's website">
+  <title>${c.header.title}</title>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;1,300;1,700&display=swap" rel="stylesheet">
+  <link rel="icon" type="image/png" href="/assets/favicon.ico"/>
+  <style>${css}</style>
+</head>
+
+<body>
+  <div id="app">
+  <div class="blog-content">
+	<a href="/" class="link"> Back to home </a>
+  	<h1 class="title">${c.header.title}</h1>
+	  <h2 class="date">${m} - ${date.getFullYear()}</h2>
+	  <div>
+	  <main>
+	  	<article>
+  		${htmlContent}
+	  	</article>
+	  </main>
+	  </div>
+  </div>
+  </div>
+</body>
+
+</html>
+`
+	fs.writeFileSync(path.join(__dirname, c.path,"index.html"), content)
 })
 
 const lastPosts = formatted.slice(0, 3)
@@ -41,20 +79,25 @@ const lastPostsMenu = lastPosts.reduce((acc, next) => {
 	// 	description+=" "+word
 	// }
 
-	return acc + `\n<div><a href="/blog/${next.path}">${next.header.title}</a><br/>-${next.header.description}</div>`
+	const date = new Date(next.header.date)
+	const m = date.toLocaleString("default", { month: "short" })
+	return acc + `\n<div><a href="/blog/${next.path}" class="with-theme-color"><small><b>${next.header.title} (${m}/${date.getFullYear()})</b></small></a><br/></div>`
 }, "")
 
 const wrapped = `
+
 <div class="last-posts-menu">
-<h3>Last posts</h3>
+	<h3>Last posts</h3>
 	<div>
 	${lastPostsMenu}
 	</div>
+	<a href="/blog"> 
+		See all...
+	</a>
+
 </div>
 `
 
 
 fs.writeFileSync(path.join(__dirname, "last_posts.html"), wrapped)
 
-
-console.log(formatted.map(x => x.header.date))
